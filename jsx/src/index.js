a4831a99364bf2799377757a641478a51be55b54
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2019-01-21 09:29:48 +0530

    Started implementing Jsx
    class vs classname in jsx and getting handy with jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = function() {
+    return (
+    <div>
+    <label className="label" for="name">Enter Name: </label>
+    <input type="text" />
+    <button style={{backgroundColor : 'blue', color: 'white'}}> Submit</button>
+    </div>
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
